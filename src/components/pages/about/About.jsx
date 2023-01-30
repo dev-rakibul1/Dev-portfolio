80f6e1d44f3b6388c0c497239ebfc75405f1b683
dev-rakibul1 <fl.rakibul@gmail.com>
2023-01-31 01:14:22 +0600

    about section updated

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -12,6 +12,7 @@ import React from "react";
 import UseTitle from "../../hook/useTitle";
 import Social from "../shared/spinner/social/Social";
 import Spinner from "../shared/spinner/Spinner";
+import "./About.css";
 
 const bull = (
   <Box
@@ -37,195 +38,241 @@ const About = () => {
   }
 
   return (
-    <Box sx={{ pt: 14, bgcolor: "#fff" }} className="about-manage">
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <Card sx={{ minWidth: 275, boxShadow: 0 }}>
-            <CardContent>
+    <Box className="about-glass">
+      <Box
+        sx={{ pt: 14, backgroundColor: "transparent" }}
+        className="about-manage"
+      >
+        <Typography
+          variant="span"
+          sx={{
+            width: 100,
+            height: 100,
+            display: "inline-block",
+            backgroundColor: "#af23f1",
+            borderRadius: "100%",
+            position: "absolute",
+            top: 0,
+            right: "33%",
+            zIndex: "-2",
+          }}
+        ></Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Card
+              // className="about-section"
+              sx={{
+                minWidth: 275,
+                boxShadow: 0,
+                // background:
+                //   "linear-gradient(135deg,rgba(255, 255, 255, 0.1),rgba(255, 255, 255, 0))",
+                // backdropFilter: "blur(15px)",
+                backgroundColor: "transparent",
+              }}
+            >
               <Typography
-                sx={{ fontSize: 14, color: "primary" }}
-                color="text.secondary"
-                gutterBottom
-              >
-                I'm a
-              </Typography>
-              <Typography
-                variant="h5"
-                component="div"
-                sx={{
-                  fontSize: "35px",
-                  fontFamily: "calibri",
-                  fontWeight: "normal",
-                  mb: -1.5,
-                  p: 0,
-                }}
-              >
-                Rakibul Islam
-              </Typography>
-              <Typography
-                sx={{
-                  mb: 1.5,
-                  fontSize: "14px",
-                  fontFamily: "calibri",
-                  fontWeight: "normal",
-                }}
-                color="text.secondary"
-              >
-                Jr. MERN stack developer
-              </Typography>
-
-              {/* PROFESSIONAL SUMMARY */}
-              <Typography
-                color="primary"
-                sx={{
-                  mb: 1.5,
-                  fontSize: "17px",
-                  fontFamily: "calibri",
-                  fontWeight: "bold",
-                }}
                 variant="span"
-              >
-                PROFESSIONAL SUMMARY
-              </Typography>
-              <Typography
-                variant="body2"
                 sx={{
-                  fontFamily: "calibri",
-                  fontWeight: "normal",
-                  fontSize: "15px",
-                  mb: 3,
+                  width: 100,
+                  height: 100,
+                  display: "inline-block",
+                  backgroundColor: "green",
+                  borderRadius: "100%",
+                  position: "absolute",
+                  bottom: 0,
+                  right: "33%",
+                  zIndex: "-10",
                 }}
-              >
-                MERN stack developer specializing in front-end development is
-                adept at building responsive web applications from front to
-                back. Passionate about learning new technologies, bringing ideas
-                to life, and working with dedicated terms to build effective and
-                robust applications suitable to the user's needs.
-              </Typography>
+              ></Typography>
+              <CardContent sx={{ color: "#fff" }}>
+                <Typography
+                  sx={{ fontSize: 14, color: "primary" }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  I'm a
+                </Typography>
+                <Typography
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    fontSize: "35px",
+                    fontFamily: "calibri",
+                    fontWeight: "normal",
+                    mb: -1.5,
+                    p: 0,
+                  }}
+                >
+                  Rakibul Islam
+                </Typography>
+                <Typography
+                  sx={{
+                    mb: 1.5,
+                    fontSize: "14px",
+                    fontFamily: "calibri",
+                    fontWeight: "normal",
+                  }}
+                  color="text.secondary"
+                >
+                  Jr. MERN stack developer
+                </Typography>
 
-              {/* SKILLS SUMMARY*/}
-              <Typography
-                color="primary"
-                sx={{
-                  mb: 1.5,
-                  fontSize: "17px",
-                  fontFamily: "calibri",
-                  fontWeight: "bold",
-                  pt: 2,
-                }}
-                variant="span"
-              >
-                SKILLS SUMMARY
-              </Typography>
-              <Typography
-                className="skill-summary"
-                variant="body2"
-                sx={{
-                  fontFamily: "calibri",
-                  fontWeight: "normal",
-                  fontSize: "15px",
-                }}
-              >
-                <span>
-                  <strong>Languages</strong> – JavaScript (ES5 + 6)
-                </span>
-                <br />
-                <span>
-                  <strong>Front-end</strong> – HTML5, CSS3, SASS.
-                </span>
-                <span>
-                  <strong>Responsive design</strong> – Flexbox, Grid, Bootstrap,
-                  Tailwind, Material UI,
-                </span>
-                <span>
-                  <strong>Client scripting</strong> – React.js, Styled
-                  components.
-                </span>
-                <span>
-                  <strong>Server Scripting</strong> – Node.js (Express),
-                  Firebase, MongoDB, Socket.io(beginner)
-                </span>
-                <span>
-                  <strong>Project management</strong> – Communication, Time
-                  management, Flexibility, Creative design, Cross browser
-                  compatibility.
-                </span>
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
+                {/* PROFESSIONAL SUMMARY */}
+                <Typography
+                  color="primary"
+                  sx={{
+                    mb: 1.5,
+                    fontSize: "17px",
+                    fontFamily: "calibri",
+                    fontWeight: "bold",
+                  }}
+                  variant="span"
+                >
+                  PROFESSIONAL SUMMARY
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    fontFamily: "calibri",
+                    fontWeight: "normal",
+                    fontSize: "15px",
+                    mb: 3,
+                    color: "#fff",
+                  }}
+                >
+                  MERN stack developer specializing in front-end development is
+                  adept at building responsive web applications from front to
+                  back. Passionate about learning new technologies, bringing
+                  ideas to life, and working with dedicated terms to build
+                  effective and robust applications suitable to the user's
+                  needs.
+                </Typography>
 
-        <Grid item xs={12} sm={6}>
-          {/* skill column */}
-          <Typography
-            variant="h4"
-            color="primary"
-            sx={{
-              textAlign: "center",
-              mb: 2,
-              fontSize: "22px",
-              textTransform: "uppercase",
-            }}
-          >
-            My Skills
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              px: 2,
-            }}
-          >
+                {/* SKILLS SUMMARY*/}
+                <Typography
+                  color="primary"
+                  sx={{
+                    mb: 1.5,
+                    fontSize: "17px",
+                    fontFamily: "calibri",
+                    fontWeight: "bold",
+                    pt: 2,
+                  }}
+                  variant="span"
+                >
+                  SKILLS SUMMARY
+                </Typography>
+                <Typography
+                  className="skill-summary"
+                  variant="body2"
+                  sx={{
+                    fontFamily: "calibri",
+                    fontWeight: "normal",
+                    fontSize: "15px",
+                    color: "#fff",
+                  }}
+                >
+                  <span>
+                    <strong>Languages</strong> – JavaScript (ES5 + 6)
+                  </span>
+                  <br />
+                  <span>
+                    <strong>Front-end</strong> – HTML5, CSS3, SASS.
+                  </span>
+                  <span>
+                    <strong>Responsive design</strong> – Flexbox, Grid,
+                    Bootstrap, Tailwind, Material UI,
+                  </span>
+                  <span>
+                    <strong>Client scripting</strong> – React.js, Styled
+                    components.
+                  </span>
+                  <span>
+                    <strong>Server Scripting</strong> – Node.js (Express),
+                    Firebase, MongoDB, Socket.io(beginner)
+                  </span>
+                  <span>
+                    <strong>Project management</strong> – Communication, Time
+                    management, Flexibility, Creative design, Cross browser
+                    compatibility.
+                  </span>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            {/* skill column */}
+            <Typography
+              variant="h4"
+              color="primary"
+              sx={{
+                textAlign: "center",
+                mb: 2,
+                fontSize: "22px",
+                textTransform: "uppercase",
+              }}
+            >
+              My Skills
+            </Typography>
             <Box
               sx={{
                 display: "flex",
-                flexWrap: "wrap",
-                gap: "10px",
+                alignItems: "center",
+                justifyContent: "center",
+                px: 2,
               }}
             >
-              {skills?.map((skill) => (
-                <Card sx={{ maxWidth: "100px" }}>
-                  <CardActionArea sx={{ position: "relative" }}>
-                    <CardMedia
-                      component="img"
-                      image={skill?.image}
-                      alt="green iguana"
-                      sx={{ maxWidth: "100%", height: "100%" }}
-                    />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: "10px",
+                }}
+              >
+                {skills?.map((skill) => (
+                  <Card sx={{ maxWidth: "100px" }}>
+                    <CardActionArea sx={{ position: "relative" }}>
+                      <CardMedia
+                        component="img"
+                        image={skill?.image}
+                        alt="green iguana"
+                        sx={{ maxWidth: "100%", height: "100%" }}
+                      />
 
-                    <CardContent>
-                      <Typography
-                        gutterBottom
-                        variant="span"
-                        sx={{
-                          display: "flex",
-                          alignItems: "start",
-                          justifyContent: "end",
-                          height: "100%",
-                        }}
-                      >
-                        {skill?.title}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-              ))}
+                      <CardContent>
+                        <Typography
+                          gutterBottom
+                          variant="span"
+                          sx={{
+                            display: "flex",
+                            alignItems: "start",
+                            justifyContent: "end",
+                            height: "100%",
+                          }}
+                        >
+                          {skill?.title}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                ))}
+              </Box>
             </Box>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Typography variant="p" sx={{ mt: 5 }}>
-              Contact us
-            </Typography>
-            <Social />
-          </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="p" sx={{ mt: 5 }}>
+                Contact us
+              </Typography>
+              <Social />
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
